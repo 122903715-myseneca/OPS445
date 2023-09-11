3b6384d9a3ec005907cc9349fc2a348d7347723f
Aleksander Savotchka <asavotchka@myseneca.ca>
2023-09-11 13:13:26 -0400

    Completed Lab 2e

---
+++ Lab2/lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# Author: Aleksander Savotchka
+# Author ID: 115894214
+# Date Created: 2023-09-11
+
+import sys
+
+timer = 10
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
+

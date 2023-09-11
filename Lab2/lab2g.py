1d098d64f5b14f660dce2f866bcaee5c64fbe996
Aleksander Savotchka <asavotchka@myseneca.ca>
2023-09-11 13:13:57 -0400

    Completed Lab 2g

---
+++ Lab2/lab2g.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# Author: Aleksander Savotchka
+# Author ID: 115894214
+# Date Created: 2023-09-11
+
+import sys
+
+if len(sys.argv) < 2:
+    timer = 3
+else:
+    timer = int(sys.argv[1])
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+print("blast off!")
+
